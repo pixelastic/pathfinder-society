0a04e028a3530868e2d6e8189938217b1b53190e
Pixelastic <tim@pixelastic.com>
2019-08-12 15:33:09 +0200

    feat(data): Dump of all 10 seasons

--- scripts/regenerate.js
+++ scripts/regenerate.js
@@ -1,10 +1,15 @@
 import helper from '../build/helper.js';
 import firost from 'firost';
+import { _, pMap } from 'golgoth';
 
 (async function() {
   helper.init();
 
-  const pages = await helper.category('Season_0_scenarios');
+  const allScenarios = [];
+  await pMap(_.range(0, 11), async seasonIndex => {
+    const scenarios = await helper.scenariosFromSeason(seasonIndex);
+    allScenarios.push(...scenarios);
+  });
 
-  await firost.writeJson(pages, './lib/data.json');
+  await firost.writeJson(allScenarios, './lib/data.json');
 })();
