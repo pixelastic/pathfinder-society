257986d798b3e70ff0ea4fc698bb35042e26142a
Pixelastic <tim@pixelastic.com>
2019-10-02 14:11:58 +0200

    fix(wiki): Update data from updated wiki

--- scripts/regenerate.js
+++ scripts/regenerate.js
@@ -1,29 +1,31 @@
 import helper from '../lib/helper.js';
 import firost from 'firost';
-import { _, pMap } from 'golgoth';
+import os from 'os';
+import { _, pMap, spinner } from 'golgoth';
 
 (async function() {
-  const maxSeasons = 11;
+  const maxSeasons = 10;
   helper.init();
-
-  firost.pulse.on('scenario', data => {
-    const seasonIndex = _.padStart(data.seasonIndex, '2', '0');
-    const scenarioIndex = _.padStart(data.scenarioIndex, '2', '0');
-    console.info(`S${seasonIndex}E${scenarioIndex}: ${data.title}`);
-  });
+  const progress = spinner(maxSeasons + 1);
 
   const allScenarios = {};
-  await pMap(_.range(0, maxSeasons), async seasonIndex => {
-    const scenarios = await helper.scenariosFromSeason(seasonIndex);
-    _.each(scenarios, scenario => {
-      const key = _.chain(scenario.title)
-        .startCase()
-        .replace(/ /g, '')
-        .camelCase()
-        .value();
-      allScenarios[key] = scenario;
-    });
-  });
+  await pMap(
+    _.range(0, maxSeasons + 1),
+    async seasonIndex => {
+      const scenarios = await helper.scenariosFromSeason(seasonIndex);
+      _.each(scenarios, scenario => {
+        const key = _.chain(scenario.title)
+          .startCase()
+          .replace(/ /g, '')
+          .camelCase()
+          .value();
+        allScenarios[key] = scenario;
+      });
+      progress.tick(`Season ${seasonIndex}`);
+    },
+    { concurrency: os.cpus().length }
+  );
 
   await firost.writeJson(allScenarios, './lib/data.json');
+  progress.succeed('./lib/data.json updated');
 })();
