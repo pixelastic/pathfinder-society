684e6319ae1ddf1bc7e9ee63d971f36503dbcd10
Pixelastic <tim@pixelastic.com>
2019-10-09 11:54:38 +0200

    chore(lint): Do not pretiffy lib/data.json

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,4 +2,7 @@
 const lintStagedConfig = require('aberlaas/build/configs/lintstaged.js');
 module.exports = {
   ...lintStagedConfig,
+  // Disable autofix of lib/data.json
+  '*.json': 'true',
+  '*.json,!lib/data.json': lintStagedConfig['*.json'],
 };
