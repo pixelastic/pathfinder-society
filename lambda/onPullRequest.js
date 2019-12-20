c05baaad3d88b07ca3677326ff8aa7e2453a55c6
Pixelastic <tim@pixelastic.com>
2019-12-20 14:33:53 +0100

    debug(function): Add debug information

--- lambda/onPullRequest.js
+++ lambda/onPullRequest.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-commonjs */
 exports.handler = function(event, context, callback) {
+  console.info(JSON.stringify(event, null, 2));
+  console.info(JSON.stringify(context, null, 2));
   callback(null, {
     statusCode: 200,
     body: 'pouet',
