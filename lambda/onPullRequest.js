872ce56b365e09156ae2988136ca361e21900eba
Pixelastic <tim@pixelastic.com>
2019-12-20 16:05:12 +0100

    feat(ci): Better output of errors

--- lambda/onPullRequest.js
+++ lambda/onPullRequest.js
@@ -2,6 +2,31 @@
 import { _ } from 'golgoth';
 
 /**
+ * Return a success response and log it
+ * @param {string} body Body of the response
+ * @returns {object} Response
+ */
+function success(body) {
+  console.info(`✔ ${body}`);
+  return {
+    statusCode: 200,
+    body,
+  };
+}
+/**
+ * Return a failure response and log it
+ * @param {string} body Body of the response
+ * @returns {object} Response
+ */
+function failure(body) {
+  console.info(`✘ ${body}`);
+  return {
+    statusCode: 500,
+    body,
+  };
+}
+
+/**
  * Triggered on every pull request closed
  * @param {object} request The triggering event
  * @returns {object} Response object
@@ -9,32 +34,35 @@ import { _ } from 'golgoth';
 export async function handler(request) {
   const debug = [];
 
-  const body = _.chain(request)
-    .get('body')
-    .thru(JSON.parse)
-    .value();
+  const rawBody = _.get(request, 'body');
+  if (_.isEmpty(rawBody)) {
+    return failure('No body passed');
+  }
+
+  let body;
+  try {
+    body = JSON.parse(rawBody);
+  } catch (err) {
+    return failure(`Body is not JSON\n${rawBody}`);
+  }
   const isMerged = _.get(body, 'pull_request.merged');
   const baseBranch = _.get(body, 'pull_request.base.ref');
   const isOnMaster = baseBranch === 'master';
   const prBranch = _.get(body, 'pull_request.head.ref');
   const isWeeklyUpdate = prBranch === 'weeklyUpdate';
 
-  const shouldTriggerRelease = isMerged && isOnMaster && isWeeklyUpdate;
+  const shouldTriggerRelease = !!(isMerged && isOnMaster && isWeeklyUpdate);
 
   debug.push(`isMerged: ${isMerged}`);
   debug.push(`baseBranch: ${baseBranch}`);
   debug.push(`prBranch: ${prBranch}`);
   debug.push(`shouldTriggerRelease: ${shouldTriggerRelease}`);
-  const displayDebug = debug.join('\n');
 
   console.info({ ...process.env });
 
   if (!shouldTriggerRelease) {
-    console.info(displayDebug);
-    return {
-      statusCode: 200,
-      body: displayDebug,
-    };
+    const displayDebug = debug.join('\n');
+    return success(displayDebug);
   }
 
   // We ping CircleCI so it triggers a new release
