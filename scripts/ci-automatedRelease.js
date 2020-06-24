e0497a5c27562cce0dd0a05b267539df22571cf9
Pixelastic <tim@pixelastic.com>
2020-06-24 09:50:35 +0200

    chore(deps): Update deps

--- scripts/ci-automatedRelease.js
+++ scripts/ci-automatedRelease.js
@@ -1,12 +1,12 @@
-const firost = require('firost');
+const run = require('firost/lib/run');
 
 (async function() {
   try {
     // Release a new version to npm
-    await firost.run('yarn run release patch');
+    await run('yarn run release patch');
 
     // Re-indexing on the Algolia index
-    await firost.run('yarn run algolia');
+    await run('yarn run indexing');
 
     process.exit(0);
   } catch (err) {
